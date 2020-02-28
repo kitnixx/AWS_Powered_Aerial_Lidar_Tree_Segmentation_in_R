9e7eac7f15b808dddd6ce70ae8b902dccc0b66d2
Katie Nicolato <47072826+kitnixx@users.noreply.github.com>
2020-02-28 15:00:48 -0800

    test commit

--- CanyonCreekLidar.R
+++ CanyonCreekLidar.R
@@ -2,9 +2,9 @@
 # Kate Nicolato, Alex Feng
 # Aerial Information Systems Laboratory, Oregon State University
 
-############
-# WORKFLOW #
-############
+#==========
+# WORKFLOW
+#==========
 
 # 1. Compiles area of interest (AOI) point cloud tiles (.las) into a catalog
 # 2. Clips catalog to AOI shapefile (.shp)
@@ -16,9 +16,9 @@
 # 8. Uses algorithm(s) to segment snags/trees - needs specific parameters
 # 9. Writes out individual snag/tree polygon shapefile (.shp, vector)
 
-###################
-# Clear workspace #
-###################
+#=================
+# Clear workspace
+#=================
 
 # Clear plots
 if(!is.null(dev.list())) dev.off()
@@ -29,9 +29,9 @@ cat("\014")
 # Clean workspace
 rm(list=ls())
 
-################
-# Dependencies #
-################
+#==============
+# Dependencies 
+#==============
 
 install.packages("lidR")
 library(lidR)
@@ -45,9 +45,9 @@ library(rgdal)
 
 library(EBImage)
 
-#########################
-# Set working directory #
-#########################
+#=======================
+# Set working directory 
+#=======================
 # Functions: setwd()
 # Inputs: Working directory file path
 # Outputs: Prints working directory file path
@@ -57,10 +57,10 @@ setwd('C:\\Users\\khnic\\Desktop\\test')
 # An alternative method to read in files
 # file <- system.file("extdata", "megaplot.laz", package="lidR")
 
-# ONLY USE FOR PRE-CLIPPED/MERGED POINT CLOUDS
-########################################################
-# Read in lidar tiles (LAS) for area of interest (AOI) #
-########################################################
+# ONLY USE FOR PRE-CLIPPED/MERGED POINT CLOUDS, OTHERWISE START WITH NEXT CODE BLOCK
+#=====================================================
+# Read in lidar tiles (LAS) for area of interest (AOI) 
+#=====================================================
 # Functions: readLAS()
 # Inputs: A point cloud dataset (.las)
 # Ouptuts: None
@@ -70,9 +70,19 @@ setwd('C:\\Users\\khnic\\Desktop\\test')
 # summary(las)
 # plot(las)
 
-######################################################
-# Create LAS tile catalog for area of interest (AOI) #
-######################################################
+# START HERE
+#=======================
+# Read in AOI shapefile 
+#=======================
+# Functions: readOGR()
+# Inputs: AOI shapefile (.shp)
+# Outputs: None
+
+aoi <- readOGR(".\\UF_S1D_unit13_obj14.shp")
+
+#====================================================
+# Create LAS tile catalog for area of interest (AOI)
+#====================================================
 # Functions: readLAScatalog()
 # Inputs: A folder of LAS tiles comprising the AOI unit
 # Outputs: A catalog of las tiles comprising the AOI unit
@@ -80,80 +90,78 @@ setwd('C:\\Users\\khnic\\Desktop\\test')
 ctg <- readLAScatalog(".\\2017_las_UpperFawn")
 plot(ctg)
 
-#########################
-# Read in AOI shapefile #
-#########################
-# Functions: readOGR()
-# Inputs: AOI shapefile (.shp)
-# Outputs: None
+# NEEDS WORK
+#====================
+# Clean point clouds 
+#====================
+# Functions:
+# Inputs:
+# Ouptuts:
 
-aoi <- readOGR(".\\UF_S1D_unit13_obj14.shp")
-print (aoi)
+#============================
+# Merge las tiles in catalog 
+#============================
+# Functions:
+# Inputs: A catalog of LAS tiles comprising the AOI
+# Outputs: A catalog of merged LAS tiles comprising the AOI
 
-##############################################
-# Clip LAS tiles in catalog to AOI shapefile #
-##############################################
+las_merge <- rbind.LAS() # (argument is LAS objects aka each tile name)
+opt_output_files(ctg) <- "folder/where/to/store/outputs/{ORIGINALFILENAME}_merged"
+
+# NEEDS WORK
+#================================================================
+# Ground filter or read in Quantum bare earth & highest hit DEMs 
+#================================================================
+# Functions: lasground()
+# Inputs:
+# Outputs: 
+# How to use Quantum bare earth and highest hit DEMs instead of making our own??
+
+#?csf
+#las_ground <- lasground(ctg,csf(class_threshold=0.2)) # classify ground points with Cloth Simulation Filter algorithm
+#plot(las_ground, color="Classification")
+
+#=======================
+# Normalize Point Cloud
+#=======================
+# Functions: lasnormalize()
+# Inputs: A catalog of merged LAS tiles comprising the AOI
+# Outputs: A normalized catalog of LAS tiles comprising the AOI
+
+las_normal <- lasnormalize() # (las_ground, tin())
+opt_output_files(ctg) <- "folder/where/to/store/outputs/{ORIGINALFILENAME}_normalized"
+plot(las_normal)
+
+#=======================================================
+# Clip normalized LAS tiles in catalog to AOI shapefile 
+#=======================================================
 # Functions: lasclip()
-# Inputs:  A catalog of LAS tiles comprising the AOI
+# Inputs:  A normalized, merged catalog of LAS tiles comprising the AOI
 # Outputs: A clipped catalog of LAS tiles comprising the AOI
 
 las_clip <- lasclip(ctg, aoi)
 opt_output_files(ctg) <- "folder/where/to/store/outputs/{ORIGINALFILENAME}_clipped"
 plot(las_clip)
 
-##############################
-# Merge las tiles in catalog #
-##############################
-# Functions:
-# Inputs: A clipped catalog of LAS tiles comprising the AOI
-# Outputs: A merged, clipped catalog of LAS tiles comprising the AOI
-
-opt_output_files(ctg) <- "folder/where/to/store/outputs/{ORIGINALFILENAME}_merged"
-
-
-#####################
-# Clean point cloud #
-#####################
-# Functions:
-# Inputs:
-# Ouptuts:
-
-##################################################################
-# Ground filter or read in Quantum bare earth & highest hit DEMs #
-##################################################################
-# Functions: lasground()
-# Inputs:
-# Outputs: 
-# How to use Quantum bare earth and highest hit DEMs instead of making our own??
-
-#########################
-# Normalize Point Cloud #
-#########################
-# Functions: lasnormalize()
-# Inputs:
-# Outputs:
-
-las_normal <- lasnormalize()
-opt_output_files(ctg) <- "folder/where/to/store/outputs/{ORIGINALFILENAME}_normalized"
-plot(las_normal)
-
-#######################
-# Segment Point Cloud #
-#######################
+#=====================
+# Segment Point Cloud 
+#=====================
 # Functions: lastrees(), lassnags(), dalponte(), silva(), watershed(), wing2015()
 # Inputs:
 # Outputs:
 
-
-
-#############################
-# Canopy Height Model (CHM) #
-#############################
-# Functions: 
-# Inputs:
+#==========================
+# Canopy Height Model (CHM) 
+#==========================
+# Functions: grid_canopy()
+# Inputs: A segmented catalog of LAS tiles comprising the AOI
 # Ouputs: Raster image (.tif) with each pixel displaying a height value (Z)
 # How to integrate Forest Service CHMs with Quantum point clouds??
 
+?grid_canopy
+chm = grid_canopy(las_normal_clip, 0.045, dsmtin()) # creates raster for canopy height model (values of cells are z relative to ground)
+plot(chm, col = height.colors(50)) # displays the chm raster
+writeRaster(chm, filename= "chm.tif", overwrite=TRUE)
 
 
 #################
@@ -193,4 +201,3 @@ writeOGR()
 # Outputs: Polygon shapefile (.shp) of individual snag polygons
 
 writeOGR()
-
