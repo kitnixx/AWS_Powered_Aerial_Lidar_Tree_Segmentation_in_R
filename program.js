eabd935c2efba72f5c6f62cdc77d4ad8c923414c
Alex Feng <35203563+gearsmotion789@users.noreply.github.com>
2020-02-27 17:57:28 -0800

    asdf

--- program.js
+++ program.js
@@ -1,15 +1,16 @@
-const shell = require('shelljs');
+const shell = require("shelljs");
 
 var ws = 30;
 var hmin = 1;
 
-for(int i=10; i<50; i+=5){
-	var script = 'C:\\Users\\nicolatk\\Desktop\\test\\segmentation_new.R'
-	var outputLocation = '';
+for (var i = 10; i < 50; i += 5) {
+  var script = "C:\\Users\\nicolatk\\Desktop\\test\\segmentation_new.R";
+  var outputLocation = "";
 
-	if (shell.exec(`Rscript ${script} ${i} ${hmin} ${outputLocation}`).code !== 0) {
-	  shell.echo('Error: IDK');
-	  shell.exit(1);
-	}
-
+  if (
+    shell.exec(`Rscript ${script} ${ws} ${hmin} ${outputLocation}`).code !== 0
+  ) {
+    shell.echo("Error: IDK");
+    shell.exit(1);
+  }
 }
