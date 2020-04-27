368661e1780c938426cb602182b4ca6a10d056de
Alex <devropes@gmail.com>
2020-04-27 15:38:26 -0700

    update

--- ec2_batch/install.R
+++ ec2_batch/install.R
@@ -1,10 +1,22 @@
-install.packages("rjson")
-install.packages("lidR")
-install.packages("raster")
-install.packages("rgdal")
-install.packages("BiocManager")
-BiocManager::install("EBImage")
-install.packages("stringr")
+#install.packages("rjson")
+#install.packages("lidR")
+#install.packages("raster")
+#install.packages("rgdal")
+#install.packages("BiocManager")
+#BiocManager::install("EBImage")
+#install.packages("stringr")
+
+
+#library(lidR)
+install.packages("parallel")
+install.packages("dplyr")
+install.packages("concaveman")
+#library(rgdal)
+install.packages("future")
+install.packages("maptools")
+
+install.packages("TreeLS")
+#library(rgdal)
 
 
 #EBImage
