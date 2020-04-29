3dde3fedc00e9f30bf2354a20d3409842889f594
devropes <devropes@gmail.com>
2020-04-29 11:59:17 -0700

    test

---
+++ backend/test.js
@@ -0,0 +1,16 @@
+const download = require('../s3_download/download.js');
+const batch = require('../ec2_batch/batch.js');
+const upload = require('../s3_upload/upload.js');
+var baseDir = process.cwd().replace('backend', '');
+baseDir = baseDir.replace(/\\/g, "/");
+
+//console.log(baseDir);
+
+batch_process('data');
+
+async function batch_process(dataFolder){
+    await download.main(baseDir, dataFolder);
+    await batch.main(baseDir, dataFolder);
+    await upload.main(baseDir, dataFolder);
+    console.log("FINISHED");
+}
